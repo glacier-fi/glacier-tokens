ccaaffd180011ea309ff171a517aa7e8a29a806d
Nelson Villagrán <nelson@v7n.cl>
2023-05-04 03:12:08 +0000

    formatting

--- test/FactoryRegistry.ts
+++ test/FactoryRegistry.ts
@@ -1,41 +1,35 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
-describe("FactoryRegistry", () => {
-
+describe('FactoryRegistry', () => {
   async function deploy() {
-    const GlacierToken = await ethers.getContractFactory("GlacierToken");
-    const glacierToken = await GlacierToken.deploy("gCLP", "gCLP", 8)
+    const GlacierToken = await ethers.getContractFactory('GlacierToken');
+    const glacierToken = await GlacierToken.deploy('gCLP', 'gCLP', 8);
 
-    const Factory = await ethers.getContractFactory("Factory");
+    const Factory = await ethers.getContractFactory('Factory');
     const factory = await Factory.deploy(glacierToken.address);
 
-    const FactoryRegistry = await ethers.getContractFactory("FactoryRegistry");
+    const FactoryRegistry = await ethers.getContractFactory('FactoryRegistry');
     const factoryRegistry = await FactoryRegistry.deploy();
 
+    return { factoryRegistry, factory };
+  }
 
-    return { factoryRegistry, factory }
-  };
-
-  describe("Events", function () {
-    it("Should emit an event on registerFactory", async function () {
+  describe('Events', function () {
+    it('Should emit an event on registerFactory', async function () {
       const { factoryRegistry, factory } = await loadFixture(deploy);
-      
-      await expect(
-        factoryRegistry.registerFactory(factory.address, 1)
-      ).to.emit(factoryRegistry, "FactoryRegistered");
+
+      await expect(factoryRegistry.registerFactory(factory.address, 1)).to.emit(factoryRegistry, 'FactoryRegistered');
     });
 
-    it("Should emit an event on unregisterFactory", async function () {
+    it('Should emit an event on unregisterFactory', async function () {
       const { factoryRegistry, factory } = await loadFixture(deploy);
       factoryRegistry.registerFactory(factory.address, 1);
-      
-      await expect(
-        factoryRegistry.unregisterFactory(factory.address)
-      ).to.emit(factoryRegistry, "FactoryUnregistered").withArgs(factory.address);
+
+      await expect(factoryRegistry.unregisterFactory(factory.address))
+        .to.emit(factoryRegistry, 'FactoryUnregistered')
+        .withArgs(factory.address);
     });
   });
-
 });
-
